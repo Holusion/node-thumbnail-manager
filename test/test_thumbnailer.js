7f59e10ca0c363376d137ce24e913db7e6db5862
Sebastien Dumetz <s.dumetz@holusion.com>
2015-10-14 18:54:45 +0200

    fixed exec command escaping

--- test/test_thumbnailer.js
+++ test/test_thumbnailer.js
@@ -10,4 +10,16 @@ describe("Thumbnailer",function(){
     }
     thumbnailer.request(__dirname+"/common.js",128).catch(done); //We're sure a thumbnail for this doesn't exists in this directory...
   });
+  it("escape shell command",function(done){
+    var thumbnailer = new Thumbnailer(__dirname);
+    thumbnailer.worker.start = function(command){
+      expect(command).to.equal('foo \\"my path\\"')
+      done();
+      return Promise.resolve("hello");
+    }
+    thumbnailer.finder.find = function(){
+      return Promise.resolve("foo %i")
+    }
+    thumbnailer.create("my path",100).catch(done);
+  });
 });
